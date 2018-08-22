2da3365cb01c020f01bfcb26989fbed50976b2dc
YehuditHaizler1 <Yehudit.Haizler@seldatinc.com>
2018-08-22 08:14:43 +0300

    21.08.2018

---
+++ 00_21.08.2018/01_matrix/matrix.js
@@ -0,0 +1,43 @@
+
+var table;
+let conditions = [
+    {
+        name: 'upperPart',
+        isValid: (i, j) => i <= j
+    },
+    {
+        name: 'downPart',
+        isValid: (i, j) => i >= j
+    },
+    {
+        name: 'diagonals',
+        isValid: (i, j) => i == 0 || i == table.children[0].children.length - 1 || j == 0 || j == table.children[0].children.length - 1
+    },
+    {
+        name: 'border',
+        isValid: (i, j) => i == 0 || j == 0 || i == table.children[0].children.length - 1 || j == table.children[0].children.length - 1
+    }
+];
+function loadTable() {
+    table = document.getElementById("table");
+}
+function resetBgc() {
+    for (var i = 0; i < table.children[0].children.length; i++) {
+        for (var j = 0; j < table.children[0].children.length; j++) {
+            table.children[0].children[i].children[j].style.backgroundColor = "gray";
+        }
+    }
+}
+function diagonals(id) {
+
+    for (var i = 0; i < table.children[0].children.length; i++) {
+        for (var j = 0; j < table.children[0].children.length; j++) {
+            if (conditions[id].isValid(i, j))
+                table.children[0].children[i].children[j].style.backgroundColor = "#3498db";
+        }
+    }
+
+    setTimeout(() => {
+        resetBgc();
+    }, 1500);
+}
